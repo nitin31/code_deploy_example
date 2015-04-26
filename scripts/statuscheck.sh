7c1dae18cb7b63d83f6e6e3364456d36344fe5de
nitin31 <nitinsharma0389@gmail.com>
2015-04-26 11:34:12 -0400

    Update statuscheck.sh

--- scripts/statuscheck.sh
+++ scripts/statuscheck.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-timePeriod=60  # time to check status  ... measured in seconds
+timePeriod=2  # time to check status  ... measured in seconds
 
 declare -a slaveAddress=('52.10.193.215' '54.149.199.57' '54.213.114.135');
 
@@ -15,16 +15,41 @@ do
     do
       echo "Testing slave ID: $slave" ;
       echo "   " ;
-      response=`curl -Is $slave | head -1`
+      response=`curl --connect-timeout 3 -Is $slave | head -1`
         if [ -z "$response" ] ; then
             echo "Alert ! $slave is down ! ";
+            echo "Stopping traffic to this server";
+            case "$slave" in
+            "52.10.193.215")
+                    sudo sed -i 's/#*BalancerMember http:\/\/52.10.193.215:80/#BalancerMember http:\/\/52.10.193.215:80/g' /etc/httpd/conf/httpd.conf
+                    ;;
+            "54.149.199.57")
+                    sudo sed -i 's/#*BalancerMember http:\/\/54.149.199.57:80/#BalancerMember http:\/\/54.149.199.57:80/g' /etc/httpd/conf/httpd.conf
+                    ;;
+            "54.213.114.135")
+                   sudo sed -i 's/#*BalancerMember http:\/\/54.213.114.135:80/#BalancerMember http:\/\/54.213.114.135:80/g' /etc/httpd/conf/httpd.conf
+                    ;;
+            esac
             echo "   " ;
             echo "$slave .... $emailContentStr" | mail -r admin-EC2@aws -s "$slave $emailSubjectStr"  $emailAddr ;
         else
            echo "Our web application is running is just fine on $slave . See response below: ";
+	   case "$slave" in
+            "52.10.193.215")
+                    sudo sed -i 's/#*BalancerMember http:\/\/52.10.193.215:80/BalancerMember http:\/\/52.10.193.215:80/g' /etc/httpd/conf/httpd.conf
+                    ;;
+            "54.149.199.57")
+                    sudo sed -i 's/#*BalancerMember http:\/\/54.149.199.57:80/BalancerMember http:\/\/54.149.199.57:80/g' /etc/httpd/conf/httpd.conf
+                    ;;
+            "54.213.114.135")
+                    sudo sed -i 's/#*BalancerMember http:\/\/54.213.114.135:80/BalancerMember http:\/\/54.213.114.135:80/g' /etc/httpd/conf/httpd.conf
+                    ;;
+           esac
+
            curl -Is $slave | head -1;
            echo "   " ;
         fi
     done
     sleep $timePeriod
 done
+
